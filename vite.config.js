688900a33e27946bee2f3aa2429f29570a60569f
osagisdevl-gisusr <noreply@osagisdevl.wcpss.net>
2025-06-09 17:12:18 -0400

    Fix base in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'app-feedback-survey-url-builder',
+  build: {
+    outDir: 'dist'
+  }
 })
